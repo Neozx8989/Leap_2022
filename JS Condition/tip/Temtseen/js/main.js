0ba4a7742cd9ee7c9e3d545c55ccd5775a6ad806
Tushigee <Tushimotot0721@gmail.com>
2022-12-03 21:35:31 +0800

    Temtsen bodlogo

---
+++ JS Condition/tip/Temtseen/js/main.js
@@ -0,0 +1,23 @@
+let a = 96;
+let b = 108;
+let c = 89;
+console.log(((a + b + c) / 3).toFixed(1));
+
+let d = 88;
+let e = 91;
+let f = 110;
+console.log(((d + e + f) / 3).toFixed(1));
+
+let TeamA = 97.6;
+let TeamB = 96.3;
+if (TeamA > TeamB) {
+    console.log("Winner Team A") 
+} else {
+    console.log("Defeat Team A")
+}
+
+let A = 97;
+let B = 97;
+if (A = B) {
+    console.log("Tentssen")
+}
