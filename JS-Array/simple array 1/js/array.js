ff03ad09f881a6c3fedafb4fd636abf23f3a7545
Tushigee <Tushimotot0721@gmail.com>
2022-12-05 20:52:09 +0800

    Update used new

---
+++ JS-Array/simple array 1/js/array.js
@@ -0,0 +1,15 @@
+let itCompanies = ["Facebook", "Googel" , "Microsoft" , "Apple" , "IBM" , "Oracle" , "Amazon"];
+console.log(itCompanies);
+
+console.log(itCompanies.length);
+
+console.log(itCompanies[0] , itCompanies[3] , itCompanies[6]);
+
+console.log(itCompanies[0]);
+console.log(itCompanies[1]);
+console.log(itCompanies[2]);
+console.log(itCompanies[3]);
+console.log(itCompanies[4]);
+console.log(itCompanies[5]);
+console.log(itCompanies[6]);
+
---
+++ JS-Array/simple array 2/js/array.js
@@ -0,0 +1,3 @@
+let itCompanies = ["Facebook", "Googel" , "Microsoft" , "Apple" , "IBM" , "Oracle" , "Amazon"];
+console.log(itCompanies[0] , itCompanies[1] , itCompanies[2]);
+console.log(itCompanies[4] , itCompanies[5] , itCompanies[6]);
