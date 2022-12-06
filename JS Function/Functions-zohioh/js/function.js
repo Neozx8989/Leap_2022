106063c442c5025ef2228b89b4c66b9e41d3c546
Tushigee <Tushimotot0721@gmail.com>
2022-12-06 19:33:51 +0800

    Used new

---
+++ JS Function/Functions-zohioh/js/function.js
@@ -0,0 +1,10 @@
+//1.Өгөгдсөн 2 тооны ихийг олох функц бич.
+let a = Number(prompt("Give me first number"));
+let b = Number(prompt("Give me second number"));
+
+let x = (a,b) => {
+    return a*b;
+}
+console.log(x(a,b));
+
+
