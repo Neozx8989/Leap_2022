ed8850ca2e5d7e58f8350281819b27942c4b13d4
Tushigee <Tushimotot0721@gmail.com>
2022-12-11 16:51:57 +0800

    Used new

---
+++ JS Loop/while-loop/js/main.js
@@ -0,0 +1,33 @@
+console.log('Natural t1oonut hewlegdeh 1-10')
+let naturalNum = prompt(`Give natural numbers 1-10`);
+for (let i = 1 ; naturalNum <=10; naturalNum++) {
+    console.log(naturalNum);
+}
+
+
+
+console.log('Sondgoi toonuud hewlegdeh 1-10')
+for(let naturalNum = 1; naturalNum <= 10; naturalNum++){
+    if(naturalNum % 2 != 0){
+        console.log(naturalNum);
+    }
+}
+
+
+
+console.log('Tegsh doonut hewlegdeh 1-10')
+for(let naturalNum = 1; naturalNum <= 10; naturalNum++){
+    if(naturalNum % 2 == 0){
+        console.log(naturalNum);
+    }
+}
+
+// console.log('1-100 hurtleh tooni niilber')
+// sum = 0
+// for(let naturalNum = 1; naturalNum <= 100; naturalNum++){
+//     if(naturalNum + sum){
+//         console.log(naturalNum );
+//     }
+// }
+
+
