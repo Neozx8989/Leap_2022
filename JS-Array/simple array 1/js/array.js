c4f817b6d856d88956d895aeb02c4dbfe124726f
Tushigee <Tushimotot0721@gmail.com>
2022-12-17 15:31:48 +0800

    Used new

--- JS-Array/simple array 1/js/array.js
+++ JS-Array/simple array 1/js/array.js
@@ -1,15 +1,43 @@
+// Array - ын хувьсагчийг itCompanies гэж зарлаж, Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon зэрэг анхны утгыг оруулна уу. 
 let itCompanies = ["Facebook", "Googel" , "Microsoft" , "Apple" , "IBM" , "Oracle" , "Amazon"];
+
+// console.log() ашиглан array хэвлэх
+console.log(`2.-----console.log() ашиглан array хэвлэх -----`)
 console.log(itCompanies);
+
+// Array дахь компаниудын length хэвлэх
+console.log(`3. -----Array дахь компаниудын length хэвлэх-----`)
 
 console.log(itCompanies.length);
 
+// Эхний компани, дунд болон сүүлчийн компанийг хэвлэх
+console.log(`4. -----Эхний компани, дунд болон сүүлчийн компанийг хэвлэх-----`)
 console.log(itCompanies[0] , itCompanies[3] , itCompanies[6]);
 
-console.log(itCompanies[0]);
-console.log(itCompanies[1]);
-console.log(itCompanies[2]);
-console.log(itCompanies[3]);
-console.log(itCompanies[4]);
-console.log(itCompanies[5]);
+// Компани бүрийг хэвлэх
+console.log(`5. -----Компани бүрийг хэвлэх-----`)
+
+console.log(itCompanies[0])
+console.log(itCompanies[1])
+console.log(itCompanies[2])
+console.log(itCompanies[3])
+console.log(itCompanies[4])
+console.log(itCompanies[5])
 console.log(itCompanies[6]);
+
+// Компанийн нэр тус бүрийг нэг нэгээр нь том үсгээр сольж хэвлэж гарга
+console.log(`6. -----Компанийн нэр тус бүрийг нэг нэгээр нь том үсгээр сольж хэвлэж гарга-----`)
+
+console.log(itCompanies[0].toLocaleUpperCase())
+console.log(itCompanies[1].toLocaleUpperCase())
+console.log(itCompanies[2].toLocaleUpperCase())
+console.log(itCompanies[3].toLocaleUpperCase())
+console.log(itCompanies[4].toLocaleUpperCase())
+console.log(itCompanies[5].toLocaleUpperCase())
+console.log(itCompanies[6].toLocaleUpperCase());
+
+
+// Array - г өгүүлбэр болгон хэвлэ: Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon зэрэг мэдээллийн технологийн томоохон компаниуд.
+console.log(`7. -----Array - г өгүүлбэр болгон хэвлэ: Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon зэрэг мэдээллийн технологийн томоохон компаниуд.-----`)
+console.log(`${itCompanies} зэрэг мэдээллийн технологийн томоохон компаниуд.`);
 
--- JS-Array/simple array 2/js/array.js
+++ JS-Array/simple array 2/js/array.js
@@ -1,3 +1,22 @@
+// Array - ын хувьсагчийг itCompanies гэж зарлаж, Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon зэрэг анхны утгыг оруулна уу.
 let itCompanies = ["Facebook", "Googel" , "Microsoft" , "Apple" , "IBM" , "Oracle" , "Amazon"];
+
+// Array - аас эхний 3 компанийг хайчилж ав
+console.log(`2 .-----Array - аас эхний 3 компанийг хайчилж ав-----`);
 console.log(itCompanies[0] , itCompanies[1] , itCompanies[2]);
+
+// Array - аас сүүлийн 3 компанийг хайчилж ав
+console.log(`3 .-----Array - аас сүүлийн 3 компанийг хайчилж ав-----`);
 console.log(itCompanies[4] , itCompanies[5] , itCompanies[6]);
+
+// Мэдээллийн технологийн дунд компани эсвэл компаниудыг arrey - аас хайчилж ав
+console.log(`4. -----Мэдээллийн технологийн дунд компани эсвэл компаниудыг arrey - аас хайчилж ав-----`);
+console.log(itCompanies[0] , itCompanies[1] , itCompanies[2] , itCompanies[4]);
+
+// Эхний мэдээллийн технологийн компанийг array - аас хас
+console.log(`5. -----Эхний мэдээллийн технологийн компанийг array - аас хас-----`);
+console.log(itCompanies[3] , itCompanies[4] , itCompanies[5] , itCompanies[6]);
+
+// Сүүлийн мэдээллийн технологийн компанийг array - аас хас
+console.log(`6. -----Сүүлийн мэдээллийн технологийн компанийг array - аас хас-----`)
+console.log(itCompanies[6] , itCompanies[5] , itCompanies[4] , itCompanies[3]);
