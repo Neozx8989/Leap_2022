0a781810edbc0d9c20ec730dd97dbd4dcc254a67
Neozx8989 <tushimotot0721@gmail.com>
2023-01-02 14:41:38 -0800

    Arrays

---
+++ JS-Array/massive-davhardsa-utga/js/main.js
@@ -0,0 +1,4 @@
+let massive =  [4, 2, 34, 4, 1, 12, 1, 4];
+for(let i=0; i<massive.length; i++) {
+
+}
---
+++ JS-String/Onoo-hewleh/js/main.js
@@ -0,0 +1,8 @@
+// const year = Number(prompt(`Heden Ond tursun be`));
+// const month = Number(prompt(`Heden sard Tursun be`));
+// const date = Number(prompt(`Heddeh udur tursun be`));
+// console.log(year  , ` - oniih`);
+// console.log(`0${month} - Sariih`)
+// console.log(`0${date} - Udriih`);
+
+
