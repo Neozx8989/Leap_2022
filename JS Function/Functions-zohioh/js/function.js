d3d96986f92d3f3efa7624e73cde896999ff8b0e
Morning <tushimotot0721@gmail.comgit config --global user.name Neozx8989egit config --global user.name Neozx8989>
2023-01-03 13:17:39 +0800

    Function assign

--- JS Function/Functions-zohioh/js/function.js
+++ JS Function/Functions-zohioh/js/function.js
@@ -3,8 +3,11 @@ let a = Number(prompt("Give me first number"));
 let b = Number(prompt("Give me second number"));
 
 let x = (a,b) => {
-    return a, b;
+    return a , b;
 }
-console.log(x(a,b));
+alert(x(a ,b));
 
+// 2. Өгөгдсөн array -аас өгөгдсөн тоо байгаа эсэхийг олох функц бич. 
+// Тухайн тоо байвал индексийг буцаана. Байхгүй бол -1 буцаана.
+let randomArr = [1, 2, 3, 4, 5, 6, 7, 8];
 
