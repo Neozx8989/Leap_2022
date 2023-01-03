8bed418b677092299fc4d81cc278a1b4f7eb1eeb
Neozx8989 <tushimotot0721@gmail.com>
2023-01-02 16:53:13 -0800

    used daalgawar

--- JS Function/Functions-zohioh/js/function.js
+++ JS Function/Functions-zohioh/js/function.js
@@ -1,13 +1,28 @@
-//1.Өгөгдсөн 2 тооны ихийг олох функц бич.
-let a = Number(prompt("Give me first number"));
-let b = Number(prompt("Give me second number"));
+// //1.Өгөгдсөн 2 тооны ихийг олох функц бич.
+// let a = Number(prompt("Give me first number"));
+// let b = Number(prompt("Give me second number"));
 
-let x = (a,b) => {
-    return a , b;
-}
-alert(x(a ,b));
+// let x = (a,b) => {
+//     return a , b;
+// }
+// alert(x(a ,b));
 
 // 2. Өгөгдсөн array -аас өгөгдсөн тоо байгаа эсэхийг олох функц бич. 
 // Тухайн тоо байвал индексийг буцаана. Байхгүй бол -1 буцаана.
 let randomArr = [1, 2, 3, 4, 5, 6, 7, 8];
+
+let x = function (randomArr) {
+    return randomArr
+}
+console.log(randomArr.indexOf(3))
+// 3. Өгөгдсөн тэмдэгт мөрөөс -аас өгөгдсөн тэмдэгт байгаа эсэхийг олох функц бич. 
+// Тухайн тэмдэгт байвал индексийг буцаана. Байхгүй бол -1 буцаана. 2doh bodlogtoi adilhan asuulttai bn?
+
+// 4. Өгөгдсөн 2 тоон интервал дахь санамсаргүй тоо буцаах функц бич.
+
+
+//5. Өгөгдсөн array - ийн дундажыг олох функц бич.
+
+let myArr = [1, 2, 3, 4, 5, 6, 7, 8]
+// 6. Өгөгдсөн array - ийн нийлбэрийг олох функц бич.
 
---
+++ JS-Methods/js/main.js
@@ -0,0 +1,2 @@
+let myNum = [4, 5, 3, 6, 1];
+console.log(myNum.reverse())
